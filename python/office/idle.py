83293040c82cf7c5af28756a0083f30a5ad3de7d
TheSaviourEking <saviour.eking@outlook.com>
2024-09-10 05:49:36 +0100

    chore: implement Phase 3: Transcription

--- python/office/idle.py
+++ python/office/idle.py
@@ -1,1 +1,20 @@
 # Your code here
+import math
+import random
+
+
+def chat():
+    coworkers = ["Jack", "Lenny", "Michelle", "Andrea"]
+    chatee = coworkers[math.floor(random.random() * 4)]
+    print("Chatting with " + chatee + "...")
+    print("Done")
+
+def getWater():
+    print("Getting water...")
+    print("That was refreshing.")
+
+def useSocialMedia():
+    socialMedia = ["FaceBook", "Twitter", "YouTube", "Reddit"]
+    choice = socialMedia[math.floor(random.random()*4)]
+    print("Using " + choice + "...")
+    print("Done")
